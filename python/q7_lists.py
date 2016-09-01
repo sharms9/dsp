ea9bd155973d50e5d07e1efd4ad93883d690cb06
sharms9 <swati.sharma9@gmail.com>
2016-09-01 12:05:40 -0400

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -96,6 +96,18 @@ def sort_last(tuples):
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
     raise NotImplementedError
+    
+#PYTHON CODE FOR sort_last(tuples):
+>>> def sort_last(tuples):
+...     print sorted (tuples, key = lambda val:val[-1])
+... 
+>>> sort_last([(1,3), (3,2), (2,1)]) 
+[(2, 1), (3, 2), (1, 3)]
+>>> sort_last([(2, 3), (1, 2), (3, 1)])
+[(3, 1), (1, 2), (2, 3)]
+>>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
+[(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+_____________________________________________________
 
 
 def remove_adjacent(nums):
