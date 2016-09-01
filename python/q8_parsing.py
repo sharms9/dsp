1f74934834a5e888b6916e7434f5b14bdf7834ba
sharms9 <swati.sharma9@gmail.com>
2016-09-01 17:06:50 -0400

    Update q8_parsing.py

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,16 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+import pandas as pd #need pandas for data structures
+def difference(frame, gfor, gagainst, result): #function statement
+    data = pd.read_csv(frame) #call the file
+    difference = abs(data[gfor] - data[gagainst]) #take ABSOLUTE VALUE of difference of goals
+    smallest = data[difference == difference.min()] #find minimum difference
+    return smallest[result].values[0]
+print
+print 'Team =', difference('football.csv', 'Goals', 'Goals Allowed', 'Team')
+>>> runfile('/Users/swatisharma/Documents/smallest_gap.py', wdir='/Users/swatisharma/Documents')
+
+Team = Aston_Villa #NOTE: If I do not take the ABSOLUTE VALUE of the difference then the answer is Leicester, question asks to simply find smallest difference so that is why abs() is implemented
+
+
