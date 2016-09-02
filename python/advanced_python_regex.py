a2ce38e43f1a12afac8f8dfd4dba4994b87a5a47
sharms9 <swati.sharma9@gmail.com>
2016-09-02 11:15:22 -0400

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,5 +1,4 @@
 #PYTHON CODE for Q1:
-from collections import Counter
 import pandas as pd
 df = pd.read_csv('faculty.csv')
 #print df
@@ -12,15 +11,27 @@ df.degree_up = df.degree.apply(lambda d: d.upper())
 #print df.degree_up
 df.deg = [x.split() for x in df.degree_up if x != '0']
 collapse_dfdeg = [i for x in df.deg for i in x]
-c = Counter(collapse_dfdeg)
+c = pd.value_counts(collapse_dfdeg)
 print
 print 'There are', len(df.degree), 'faculty members in the Biostats department.'
 print
-print 'The degree types and tally are:', c
+print 'The degree types and tally are:'
+print c
 
 #OUTPUT for Q1:
-#(>>> runfile('/Users/swatisharma/Documents/find_degrees.py', wdir='/Users/swatisharma/Documents')
-#
-#There are 37 faculty members in the Biostats department.
-#
-#The degree types and tally are: Counter({'PHD': 31, 'SCD': 6, 'MPH': 2, 'MS': 2, 'MD': 1, 'MA': 1, 'BSED': 1, 'JD': 1})
+>>> runfile('/Users/swatisharma/Documents/q1.py', wdir='/Users/swatisharma/Documents')
+
+There are 37 faculty members in the Biostats department.
+
+The degree types and tally are:
+PHD     31
+SCD      6
+MPH      2
+MS       2
+BSED     1
+JD       1
+MA       1
+MD       1
+dtype: int64
+__________________________________
+
