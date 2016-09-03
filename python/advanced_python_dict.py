e51200f1df7205cf77cecdabf3971edd15d95b67
sharms9 <swati.sharma9@gmail.com>
2016-09-03 01:18:33 -0400

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -17,3 +17,24 @@ print first3kv
 
 {'Putt': [' PhD ScD', 'Professor of Biostatistics', 'mputt@mail.med.upenn.edu'], 'Feng': [' Ph.D', 'Assistant Professor of Biostatistics', 'ruifeng@upenn.edu'], 'Bilker': ['Ph.D.', 'Professor of Biostatistics', 'warren@upenn.edu']}
 _______________________________________
+#PYTHON CODE for Q7:
+import pandas as pd
+import csv
+
+df = pd.read_csv('faculty.csv')
+faculty_dict={}
+df.name = df[df.columns[0]]
+df.lname = df.name.apply(lambda x: x.split(' ')[-1])
+df.fname = df.name.apply(lambda x: x.split(' ')[0])
+df.fl = df.fname + ', ' + df.lname
+faculty_dict = df.set_index(df.fl).T.to_dict('list')
+
+print
+first3kv = {k: faculty_dict[k] for k in faculty_dict.keys()[:3]}
+print first3kv
+
+#OUTPUT for Q7:
+>>> runfile('/Users/swatisharma/Documents/q7.py', wdir='/Users/swatisharma/Documents')
+
+{'Sarah, Ratcliffe': ['Sarah Jane Ratcliffe', ' Ph.D.', 'Associate Professor of Biostatistics', 'sratclif@upenn.edu'], 'Yenchih, Hsu': ['Yenchih Hsu', ' Ph.D.', 'Assistant Professor of Biostatistics', 'hsu9@mail.med.upenn.edu'], 'J., Landis': ['J. Richard Landis', ' B.S.Ed. M.S. Ph.D.', 'Professor of Biostatistics', 'jrlandis@mail.med.upenn.edu']}
+__________________________________________
