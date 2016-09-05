435f4d008cb2f241da32a14214a54ae368c3eab6
sharms9 <swati.sharma9@gmail.com>
2016-09-04 20:17:19 -0400

    Update matrix_algebra.py

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -1,3 +1,19 @@
-# Matrix Algebra
+# Linear Algebra 1.1):
+A = [(1,2,3),(2,7,4)]   # matrix
+print
+print 'Matrix A:', A    # output matrix
+print
+rows = len(A)           # find number of rows in matrix
+columns = len(A[0])     # find number of columns in matrix
+print 'Dimension of matrix A:',rows,"x",columns       # output dimensions
+print
 
-PLACE YOUR CODE HERE
+# Output of 1.1):
+# >>> runfile('/Users/swatisharma/Documents/LA1p1.py', wdir='/Users/swatisharma/Documents')
+#
+# Matrix A: [(1, 2, 3), (2, 7, 4)]
+#
+# Dimension of matrix A: 2 x 3
+
+#__________________________________________________________________
+
